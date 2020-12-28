f856c19763399ddb3452a6d6b93364cc85f1034f
Jonathan Thai <36181811+jonathanht@users.noreply.github.com>
2020-12-28 11:41:31 -0800

    Created & added enemybullet script to prefab

---
+++ Solarius Game/Assets/Scripts/EBulletScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EBulletScript : MonoBehaviour
+{
+    public float speed = 15f;
+    public Rigidbody2D EBBod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        EBBod.velocity = transform.right * speed;
+    }
+}
