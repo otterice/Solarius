70a290de47005eb82571b6f6afe51754743ad67e
otterice <josephdang101@gmail.com>
2020-12-28 20:11:41 -0800

    added new player bullet, added a gameobject destroyer

---
+++ Solarius Game/Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjects : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Destroy(collision.gameObject);
+    }
+}
