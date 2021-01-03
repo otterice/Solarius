978e7fbcf690060fa8bf2256963d0fafff80f05b
Spacekevii <47936842+Spacekevii@users.noreply.github.com>
2021-01-03 00:54:38 -0800

    Fixed EnemySpawner and Pathing
    Massive Gamebreaking bug. NEED TO FIX ASAP

--- Solarius Game/Assets/Scripts/EnemySpawner.cs
+++ Solarius Game/Assets/Scripts/EnemySpawner.cs
@@ -6,28 +6,21 @@ public class EnemySpawner : MonoBehaviour
 {
 
     public GameObject enemy;
-    public float posX;
-    public float posY;
-    public float spawnTime = 3f;
-    [SerializeField] int counter = 0;
+    public float posX1;
+    public float posX2;
+    public float posY1;
+    public float posY2;
+    public float spawnTime = 8f;
 
     void Start()
     {
-        InvokeRepeating("Spawner", spawnTime, 1);
+        InvokeRepeating("Spawner", 3, spawnTime);
     }
 
     void Spawner()
     {
-        if (counter >= 10)
-        {
-            CancelInvoke();
-        }
-        else
-        {
-            posX = Random.Range(-27.77f, -19.22f); 
-            posY = Random.Range(0.12f, 8.0f);
+            float posX = Random.Range(posX1, posX2); 
+            float posY = Random.Range(posY1, posY2);
             Instantiate(enemy, new Vector2(posX, posY), Quaternion.identity);
-            counter++;
-        }
     }
 }
